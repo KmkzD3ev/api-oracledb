535272571f5055c1a435c3488926a20235c187b8
kmkz <eduardomoura228@gmail.com>
2025-02-03 09:24:01 -0300

    Adicionando rota raiz para exibir status da API

--- APPQUERY/server.js
+++ APPQUERY/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const executeQuery = require('./api/execute-query'); // Importa a API
 
+// Rota principal para verificar se a API está rodando
+app.get('/', (req, res) => {
+    res.send('API está rodando! Acesse <a href="/api/execute-query">/api/execute-query</a> para consultar os dados.');
+});
+
+// Rota da API para executar a consulta
 app.get('/api/execute-query', executeQuery);
 
 const PORT = process.env.PORT || 3000;
